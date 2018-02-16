7064eca8ef51c443e5be9829634a4073f7783a6f
Evan Taylor <etaylor8086@gmail.com>
2018-02-16 00:03:34 -0800

    Support registering users on server, update dependencies and add proxy config for Angular development

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+// Properties
+const saltRounds = 10;
+
 // Initialize database object
 var db = require('../queries');
 
 // Define routes
 router.get('/', getAllUsers);
 router.get('/:username', getSingleUser);
+router.post('/register', registerUser);
 
 
 // Router functions
 function getAllUsers(req, res, next) {
-  db.any('select * from users')
+  const obj = {
+    columns: ['id', 'name', 'isenabled', 'userlevel', 'twitter', 'twitch', 'youtube']
+  };
+
+  db.any('select ${columns:name} from users', obj)
     .then(function (data) {
       res.status(200)
         .json({
@@ -26,19 +34,43 @@ function getAllUsers(req, res, next) {
 }
 
 function getSingleUser(req, res, next) {
-  var userName = req.params.username;
-  db.one('select * from users where LOWER(name) = LOWER($1)', userName)
+  const obj = {
+    columns: ['id', 'name', 'isenabled', 'userlevel', 'twitter', 'twitch', 'youtube'],
+    username: req.params.username
+  };
+
+  db.one('select ${columns:name} from users where lower(name) = lower(${username})', obj)
     .then(function (data) {
       res.status(200)
         .json({
           status: 'success',
           data: data,
-          message: 'Retrieved ONE user'
+          message: 'Returned user'
         });
     })
     .catch(function (err) {
       return next(err);
     });
+}
+
+function registerUser(req, res, next) {
+  const bcrypt = require('bcrypt');
+
+  // Hash password with bcrypt before storing in database
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    req.body.password = hash;
+    db.none('insert into users (${this:name}) values (${this:csv})', req.body)
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Registered user'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+  });
 }
 
 module.exports = router;
