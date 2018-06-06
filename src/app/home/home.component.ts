4e844a4d22f9c7abf6330309b21b8e0f2685c5cd
Evan Taylor <etaylor8086@gmail.com>
2018-06-06 00:11:36 -0700

    Add more articles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,24 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { RecordsService } from '../services/records.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  latestRecords: object[] = [];
-  constructor(private recordsService: RecordsService) { }
+    latestRecords: object[] = [];
+    constructor(private recordsService: RecordsService) { }
 
-  ngOnInit() {
-    this.recordsService.getLatestRecords().subscribe(
-      res => {
-        if (res['success']) {
-          this.latestRecords = res['data'];
-        }
-      },
-      err => {
-      }
-    );
-  }
+    ngOnInit() {
+    }
 
 }
