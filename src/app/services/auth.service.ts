616480b666d8e34f931bc84d64394d2331b715b1
Evan Taylor <etaylor8086@gmail.com>
2019-01-08 12:45:13 -0800

    Support Angular 6

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Subject } from 'rxjs/Subject';
+import { Subject } from 'rxjs';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable()
