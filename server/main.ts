a91d18b3811ad0ab28e4449ee686118f8caeb50f
Evan Taylor <etaylor8086@gmail.com>
2019-01-08 23:16:56 -0800

    Allow server to gracefully shut down

--- server/main.ts
+++ server/main.ts
@@ -9,11 +9,11 @@ const app = express();
 let config;
 
 try {
-    config = require('../config.json');
+  config = require('../config.json');
 } catch (e) {
-    Utilities.generateConfig();
-    console.log('You will need to configure your database credentials in this file before running the server.');
-    process.exit(e.code);
+  Utilities.generateConfig();
+  console.log('You will need to configure your database credentials in this file before running the server.');
+  process.exit(e.code);
 }
 
 import { ApiController } from './controllers/api';
@@ -36,16 +36,16 @@ app.use('/api', new ApiController().router);
 
 // Error handler
 app.use((err, req, res, next) => {
-    res.status(err.status || 500)
-        .json({
-            success: false,
-            message: err.message
-        });
+  res.status(err.status || 500)
+    .json({
+      success: false,
+      message: err.message
+    });
 });
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 // Set Port
@@ -53,4 +53,35 @@ const port = config.server.port;
 app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port, () => console.log(`metroidprime.run server running on localhost:${port}`));
+
+server.listen(port, () => {
+  console.log(`metroidprime.run server running on localhost:${port}`);
+});
+
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
+process.on('SIGUSR2', shutDown); // nodemon restart
+
+let connections = [];
+
+server.on('connection', connection => {
+  connections.push(connection);
+  connection.on('close', () => connections = connections.filter(curr => curr !== connection));
+});
+
+function shutDown() {
+  console.log('Received kill signal, shutting down');
+
+  server.close(() => {
+    console.log('Closed out remaining connections');
+    process.exit(0);
+  });
+
+  setTimeout(() => {
+    console.error('Could not close connections in time, forcefully shutting down');
+    process.exit(1);
+  }, 10000);
+
+  connections.forEach(curr => curr.end());
+  setTimeout(() => connections.forEach(curr => curr.destroy()), 5000);
+}
