8e6693831c2e492708ce8e98d12685a3dcc3aceb
Evan Taylor <etaylor8086@gmail.com>
2019-02-10 14:45:49 -0800

    Fix game wildcard permissions

--- server/db/data/permissions.sql
+++ server/db/data/permissions.sql
@@ -1,4 +1,4 @@
 INSERT INTO permissions (id, value) VALUES (1, 'users.*');
-INSERT INTO permissions (id, value) VALUES (2, 'games.*');
+INSERT INTO permissions (id, value) VALUES (2, 'game.*');
 INSERT INTO permissions (id, value) VALUES (3, 'news.*');
 INSERT INTO permissions (id, value) VALUES (4, 'leaderboards.*');
