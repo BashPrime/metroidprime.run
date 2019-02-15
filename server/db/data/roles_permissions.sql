818592e22ed9d3a38d81f0889fc43e9c7340c92e
Evan Taylor <etaylor8086@gmail.com>
2019-02-14 23:19:57 -0800

    Add create, update article permissions for moderators

--- server/db/data/roles_permissions.sql
+++ server/db/data/roles_permissions.sql
@@ -2,3 +2,6 @@ INSERT INTO roles_permissions (roleid, permissionid) VALUES (1, 1);
 INSERT INTO roles_permissions (roleid, permissionid) VALUES (1, 2);
 INSERT INTO roles_permissions (roleid, permissionid) VALUES (1, 3);
 INSERT INTO roles_permissions (roleid, permissionid) VALUES (1, 4);
+INSERT INTO roles_permissions (roleid, permissionid) VALUES (2, 5);
+INSERT INTO roles_permissions (roleid, permissionid) VALUES (2, 6);
+
