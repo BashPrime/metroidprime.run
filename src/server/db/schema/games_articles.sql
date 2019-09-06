f03de98be24a55b230fdc49aff43f7bb5074e872
Evan Taylor <etaylor8086@gmail.com>
2019-09-05 22:27:55 -0700

    Fix typo my dog made
    Seriously, she stepped on my keyboard and I committed her changes!

--- src/server/db/schema/games_articles.sql
+++ src/server/db/schema/games_articles.sql
@@ -5,7 +5,7 @@ CREATE TABLE games_articles (
     description VARCHAR(250) NULL,
     content TEXT NOT NULL,
     categoryid INTEGER NOT NULL REFERENCES games_articles_categories(id),
-    last_updated_us45rer INTEGER NOT NULL REFERENCES users(id),
+    last_updated_user INTEGER NOT NULL REFERENCES users(id),
     last_updated_date TIMESTAMP NOT NULL,
     gameid INTEGER NOT NULL REFERENCES games(id)
 );
